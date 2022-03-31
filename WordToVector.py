5117f55ec04e196f223bdaa31d14e8ea9a664c9f
samuellau0802 <82603501+samuellau0802@users.noreply.github.com>
2022-03-31 22:38:45 +0800

    added WordToVector.py

--- WordToVector.py
+++ WordToVector.py
@@ -8,10 +8,24 @@ def tfidf(df, max_df=1.0, min_df=1):
     :param max_df: sklearn tfidf parameters. default=1.0. ignore terms that have a document frequency strictly higher than the given threshold
     :param min_df: sklearn tfidf parameters. default=1. ignore terms that have a document frequency strictly lower than the given threshold
 
-    :return a nxb matrix, where b equals to the number of unique vocabulary
+    :return a n x b matrix, where b equals to the number of unique vocabulary
+    :return a dictionary of vocabulary that maps the terms to feature indices.
     '''
 
     vectorizer = TfidfVectorizer(max_df=max_df, min_df=min_df)
     X = vectorizer.fit_transform(list(df['tokenized_text']))
-    X = X.todense()
+    X = X.todense() # sprase to dense matrix
     return X, vectorizer.vocabulary_
+
+def transformer(df, model):
+    '''
+    sentence transformer converts the dataframe with raw text into a matrix of word embeddings. supports multi-GPU Encoding
+
+    :param df: a dataframe that contains text columns, with n rows
+    :param model: pretrained model that you would like to use
+
+    :return a n x 728 matrix
+    '''
+
+    embeddings = model.encode(list(df['text']), device='cuda', show_progress_bar=True)
+    return embeddings
